b363f706d5142166dd1080f73d222efc58999e80
Mohamed Mousa <mousa1152003@gmail.com>
2024-09-23 17:32:42 +0300

    create splash screen

---
+++ lib/features/const.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xff6B5FF8);
+const Color whiteColor = Color(0xffFFFFFF);
+const Color blackColor = Color(0xff000000);
+const Color redColor = Color(0xffEF476F);
